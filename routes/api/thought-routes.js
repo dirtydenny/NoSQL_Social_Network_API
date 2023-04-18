87c85c7b630f902a6bf83cc07a2727921ad614b6
dirtydenny <dallen7@socal.rr.com>
2023-04-18 09:59:08 -0700

    use user routes to help create thought routes

--- routes/api/thought-routes.js
+++ routes/api/thought-routes.js
@@ -1,1 +1,21 @@
 const router = require("express").Router();
+const {
+  getAllThoughts,
+  getSingleThought,
+  createThought,
+  removeThought,
+  updateThought,
+  createReaction,
+  removeReaction,
+} = require("../../controllers/thought-controller");
+
+// get all thoughts
+router.route('/').get(getAllThoughts).post(createThought);
+// get a single thought by id 
+router.route('/:thoughtId').get(getSingleUser).delete(deleteUser).put(updateUser);
+// reactions
+router.route('/:thoughtId/reactions').post(createReaction);
+// remove reaction
+router.route('/:thoughtId/reactions/:reactionId').delete(removeReaction);
+
+module.exports = router;
